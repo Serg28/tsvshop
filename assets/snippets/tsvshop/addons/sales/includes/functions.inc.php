393d847bc4ef5251a8bcee2690306d02d2630dc0
Sergey <privat_tel@mail.ru>
2017-09-08 13:37:19 +0300

    v5.4.2 Исправлена опечатка

--- assets/snippets/tsvshop/addons/sales/includes/functions.inc.php
+++ assets/snippets/tsvshop/addons/sales/includes/functions.inc.php
@@ -1,7 +1,7 @@
 <?php
 //if (!IN_TSVSHOP_MODE) {die();}
 //if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODx Content Manager instead of accessing this file directly.");
-$tables['sales']="system";  
+$tables['sales']="system";
 $tsvshop['dborders']=$modx->getFullTableName('shop_order');
 $tsvshop['dborders_details']=$modx->getFullTableName('shop_order_detail');
 $tsvshop['tplmailupdateorder'] = !empty($tplmailupdateorder) ? $tplmailupdateorder : "Shop_UpdateOrder";
@@ -31,7 +31,7 @@ function parsetable($res,$filename) {
         $lang = isset($lang) ? $lang : $modx->config['manager_language'];
         //$filename=$modx->config['base_path'].'assets/snippets/tsvshop/admin/tpl/saletable.tpl';
         $tpl=get_file_contents($filename);
-        
+
         $url="index.php";
         $row1=array('url'      => $url,
                     'modulea'  => $modulea,
@@ -39,15 +39,15 @@ function parsetable($res,$filename) {
                     'theme'    => $theme,
                     'mgrdir'  => MGR_DIR
         );
-        
+
         $tpltr = getStr($tpl, '<!--repeat-->', '<!--/repeat-->');
-        
+
         $row = array_merge($shop_lang,$row1);
-        
+
         if (!empty($res) && !empty($tpl)) {
         	while($rows=$modx->db->getRow($res)) {
                   $row = array_merge($row,$rows);
-                  
+
                     $temp = str_replace('[+moduleid+]', $_GET['id'], $tpltr);
                     $temp = str_replace('[+lang+]', $lang, $temp);
                     foreach ($rows as $key => $value) {
@@ -62,10 +62,10 @@ function parsetable($res,$filename) {
                         if ($key=="status") {
                           $temp= str_replace('[+statussel+]','<select name="status" id="chst'.$row['numorder'].'" onchange="chst(\''.((int)$_GET['id']).'\',\''.((int)$_GET['a']).'\',\'/'.MGR_DIR.'/index.php\',document.getElementById(\'chst'.$row['numorder'].'\'),\''.$row['numorder'].'\',updst);return false">'.buildstatus($value, explode("||",$tsvshop['StatusOrder'])).'</select>',$temp);
                         }
-                       
+
                         $status = explode("||",$tsvshop['StatusOrder']);
                         foreach ($status as $tmp) {
-                          $tmpstatus = explode("==",$tmp); 
+                          $tmpstatus = explode("==",$tmp);
                           if ($key=="status" && $value==$tmpstatus[0]) {
                             $temp = str_replace('[+color+]', ' style="background:#'.$tmpstatus[1].'" ', $temp);
                           }
@@ -184,7 +184,7 @@ function updateorder($idorder) {
     $output_sales_error="";
     $act=$_GET['act'];
     $subtotal = 0;
-    
+
     $folders = $cache->cache('folders','tsvshop');
     if (!is_array($tsvshop['sysfields'])) $tsvshop['sysfields'] = explode(',',$tsvshop['sysfields']);
     if (!is_array($tsvshop['customfields'])) $tsvshop['customfields'] = explode(',',$tsvshop['customfields']);
@@ -195,7 +195,7 @@ function updateorder($idorder) {
          $tsvshop['sysfields'] = array_merge($tsvshop['sysfields'],$tsvshop['cf_'.$folder]);
       }
     }
-    
+
     if ($user=="manager") {
         if (!empty($act) && $act=="updateorder" && !empty($idorder) && is_numeric(intval($idorder)) && $idorder !="0") {
           // ищем данные о товарах в REQUEST  v5.4.1----
@@ -220,9 +220,9 @@ function updateorder($idorder) {
           }
           //пересчет и добавление итоговых сумм  ---v5.4.1----
           $fields['subtotal'] = $subtotal;
-          $fields['discountsize'] = tsv_PriceFormat((($fields['subtotal']*$fields['discount'])/100);
+          $fields['discountsize'] = tsv_PriceFormat(($fields['subtotal']*$fields['discount'])/100);
           $fields['total'] = tsv_PriceFormat(($fields['subtotal']+$fields['shipping']+$fields['nalog'])-$fields['discountsize']);
-          
+
           //------
           //updateMail($modx->db->escape($_GET['status']),$idorder);
           if ($modx->db->update( $fields, $tsvshop['dborders'], 'numorder = "' . intval($idorder) . '"' )) {
@@ -232,7 +232,7 @@ function updateorder($idorder) {
               return "<span class='ok'>".$shop_lang['sales_update_ok']."</span>";
           } else {
               return "<span class='error'>".$shop_lang['sales_update_error']."</span>";
-          }		
+          }
        }
     }
 }
@@ -264,7 +264,7 @@ function updstorder($idorder) {
              if ($result) {
                   $status = explode("||",$tsvshop['StatusOrder']);
                   foreach ($status as $tmp) {
-                    $tmpstatus = explode("==",$tmp); 
+                    $tmpstatus = explode("==",$tmp);
                       if ($_GET['status']==$tmpstatus[0]) {
                         $color = $tmpstatus[1];
                       }
@@ -272,10 +272,10 @@ function updstorder($idorder) {
                   //Запускаем событие TSVshopOnOrderStatusUpdate
                   $modx->invokeEvent("TSVshopOnOrderStatusUpdate",array("idorder"=>$idorder, "newstatus"=>$modx->db->escape($_GET['status'])));
                   return $idorder."||".$color."||success";
-        
+
              } else {
 				          return $idorder."||".$color."||error";
-             }		
+             }
         }
     }
 }
@@ -301,8 +301,8 @@ function sendMailUpdate($emails, $subject='', $body, $isHTML=false)
 		$modx->mail->From = $tsvshop['SmtpFromEmail'];
 		$modx->mail->FromName = $tsvshop['SmtpFromName'];
 		$modx->mail->Subject = $subject;
-    
-    		
+
+
 		$from = explode(',',$tsvshop['SmtpFromEmail']);
         	if (is_array($from)) {
 			     $modx->mail->From = trim($from[0]);
@@ -310,7 +310,7 @@ function sendMailUpdate($emails, $subject='', $body, $isHTML=false)
 			     $modx->mail->AddReplyTo(trim($from[0]), $modx->config['site_name']);
 		    } else {
 			     $modx->mail->From = trim($tsvshop['SmtpFromEmail']);
-			     $modx->mail->Sender   = trim($tsvshop['SmtpFromEmail']); 
+			     $modx->mail->Sender   = trim($tsvshop['SmtpFromEmail']);
 			     $modx->mail->AddReplyTo(trim($tsvshop['SmtpFromEmail']), $modx->config['site_name']);
 		    }
 
@@ -327,11 +327,11 @@ function sendMailUpdate($emails, $subject='', $body, $isHTML=false)
 			}
 		}
 		elseif(is_string($emails)) {
-		  $modx->mail->AddAddress($emails); 
+		  $modx->mail->AddAddress($emails);
     }
     //$mail->SetFrom($tsvshop['SmtpFromEmail'], $tsvshop['SmtpFromName']); //от кого (желательно указывать свой реальный e-mail на используемом SMTP сервере
   	$modx->mail->AddReplyTo($tsvshop['SmtpReplyEmail'], $tsvshop['SmtpFromName']);
-		
+
 		return ($modx->mail->Send() ? true : false);
 	}
 
@@ -354,11 +354,11 @@ function updateMail($newstatus,$idorder) {
             $body = str_replace("[+shop.order.num+]",$i,$body);
             $i++;
         }
-    }  
+    }
     $body = str_replace("[+shop.order.sitename+]",$modx->config['site_name'],$body);
     if ($row['status']!=$newstatus) {
       if (sendMailUpdate(DeCryptMessage($row['email'], $tsvshop['SecPassword']), $tsvshop['SubjectUpdateStatus'], $body, 'true')) {return true;} else {return false;}
-    }   
+    }
 }
 
 
@@ -375,11 +375,11 @@ function vieworder($filename) {
     $id=_filter($_GET['idorder'],1);
     $filename = (empty($filename)) ? TSVSHOP_PATH.'addons/sales/tpl/orderview.tpl' : $filename;
     if ($user=="manager") {
-      if (!empty($act) && $act=="vieworder" && $tables['sales']!="none" && $tsvshop['dborders']!="" && !empty($id) && is_numeric($id)) { 
-        if ($res = $modx->db->select('*', $tsvshop['dborders'], 'numorder = "' . $id . '"','numorder','1' )) { 
+      if (!empty($act) && $act=="vieworder" && $tables['sales']!="none" && $tsvshop['dborders']!="" && !empty($id) && is_numeric($id)) {
+        if ($res = $modx->db->select('*', $tsvshop['dborders'], 'numorder = "' . $id . '"','numorder','1' )) {
            $row = $modx->db->getRow($res);
            $url="index.php";
-            
+
            $tpl=get_file_contents($filename);
            $row1=array('moduleurl'      => $url,
                        'modulea'  => $modulea,
@@ -387,9 +387,9 @@ function vieworder($filename) {
                        'theme'    => $theme,
                        'mgrdir'  => MGR_DIR
            );
-           
+
            $tpltr = getStr($tpl, '<!--repeat-->', '<!--/repeat-->');
-           
+
            $row = array_merge($shop_lang,$row1,$row);
            foreach ($row as $key => $value) {
              if (in_array($key,explode(",",$tsvshop['SecFields'])))  {
@@ -400,7 +400,7 @@ function vieworder($filename) {
              }
              if ($key=="status") {
                $tpl = str_replace('[+buildstatus+]', '<select name="status" id="status">'.buildstatus($value, explode("||",$tsvshop['StatusOrder'])).'</select>', $tpl);
-             }  
+             }
              if ($key=="shipping") {$shipping = $value;}
              if ($key=="nalog") {$nalog = $value;}
              if ($key=="discount") {$discount = $value;}
@@ -408,15 +408,15 @@ function vieworder($filename) {
                 if (!empty($value)) $tpl = str_replace('[+'.$key.'+]', $value, $tpl);
              }
            }
-           
+
            if ($res = $modx->db->select('*', $tsvshop['dborders_details'], 'numorder = "' . $id . '"','numorder' )) {
-             
+
              while ($order = $modx->db->getRow($res))  {
                $row = array_merge($row,$order);
 			         $r++;
                $temp = str_replace('[+moduleid+]', $_GET['id'], $tpltr);
                foreach ($order as $key => $value) {
-                 
+
                  if (!empty($value)) $temp = str_replace('[+'.$key.'+]', $value, $temp);
                  if ($key == 'price') {
                     $summa = $value*$order['quantity'];
@@ -426,9 +426,9 @@ function vieworder($filename) {
                }
                $temp = str_replace('[+num+]', $r, $temp);
                $out.=$temp;
-             }  
-             $out = str_replace($tpltr,$out,$tpl);       
-             
+             }
+             $out = str_replace($tpltr,$out,$tpl);
+
              //подсчет и заполнение итоговых сумм
              $discountsize = ($subtotal*$discount)/100;
              $total = ($subtotal+$shipping+$nalog)-$discountsize;
@@ -437,18 +437,18 @@ function vieworder($filename) {
              $out = str_replace('[+topay+]', tsv_PriceFormat($total), $out);
              $out = str_replace('[+subtotal+]', tsv_PriceFormat($subtotal), $out);
              $out = str_replace('[+discountsize+]', tsv_PriceFormat($discountsize), $out);
-                          
+
              $out = preg_replace('/(\[\+.*?\+\])/' ,'', $out);
              return $out;
            }
         }
         //---
       }
-    
+
     }
 }
 
-$snotice=""; 
+$snotice="";
 
 switch ($_GET['act']) {
     case 'delorder':
--- install/assets/plugins/TSVshopNotifier.tpl
+++ install/assets/plugins/TSVshopNotifier.tpl
@@ -1,33 +1,49 @@
 /**
  * TSVshopNotifier
  *
- * Отображение новых заказов модуля TSVshop на главной странице админки MODx (TSVshopNotifier plugin for MODx Evolution)
+ * Оповещения от модуля TSVshop на главной странице админки MODx (TSVshopNotifier plugin for MODx Evolution)
  *
- * @author    Telnij Sergey (Serg24) <privat_tel@mail.ru>, http://tsvshop.xyz
+ * @author    Telnij Sergey (Serg24) <tsv.art.com@gmail.com>, http://tsvshop.xyz
  * @category  plugin
- * @version 	1.0
+ * @version 	1.1
  * @license 	http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
- * @internal	@events OnManagerWelcomePrerender
+ * @internal	@events OnManagerWelcomeHome
  * @internal	@modx_category TSVshop
  * @internal  @installset base
  */
 
-$output = "";
 $e = &$modx->Event;
-if($e->name == 'OnManagerWelcomePrerender'){
+switch($e->name){
+    case 'OnManagerWelcomeHome':
+		$count = $modx->db->getValue(
+			$modx->db->select("COUNT(*)",$modx->getFullTableName('shop_order'),"status='Новый'")
+		);
+		$module_id = $modx->db->getValue(
+			$modx->db->select("id",$modx->getFullTableName('site_modules'),"name='TSVshop'")
+		);
+		if (!empty($count)){
+			$body = 'Новых заказов: <a href="/'.MGR_DIR.'/index.php?a=112&id='.$module_id.'">'.$count.'</a>';
+		} else {
+			$body = 'Новых заказов нет';
+		}
+                                
+        $widgets['neworders'] = array(
+            'menuindex' =>'3',
+            'id' => 'neworders',
+            'cols' => 'col-sm-12',
+            'icon' => 'fa-shopping-bag',
+            'title' => 'Новые заказы',
+            'body' => '<div class="card-body">'.$body.'</div>'
+        );
+	/*$widgets['shopstat'] = array(
+            'menuindex' =>'4',
+            'id' => 'shopstat',
+            'cols' => 'col-sm-6',
+            'icon' => 'fa-rss',
+            'title' => 'Статистика',
+            'body' => '<div class="card-body">Статистика заказов</div>'
+        );*/
+        $e->output(serialize($widgets));
+    break;
+}
 
-$count = $modx->db->getValue(
-$modx->db->select("COUNT(*)",$modx->getFullTableName('shop_order'),"status='Новый'")
-);
-$module_id = $modx->db->getValue(
-$modx->db->select("id",$modx->getFullTableName('site_modules'),"name='TSVshop'")
-);
-if (!empty($count)){
-$output = '<div class="sectionHeader" style="color:red">Новые заказы:</div>
-<div class="sectionBody">Новых заказов: <a href="/'.MGR_DIR.'/index.php?a=112&id='.$module_id.'">'.$count.'</a></div>';
-} else {
-$output = '<div class="sectionHeader" style="color:red">Новые заказы:</div>
-<div class="sectionBody">Новых заказов нет</div>';
-}
-$e->output($output);
-}
