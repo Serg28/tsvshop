e9aa08827958303af6132f510c4b4ac0267a1656
Sergey <privat_tel@mail.ru>
2018-12-13 11:56:31 +0200

    v5.4.5 Правка аддона Аддоны
    Кнопка Обновить и проверка прав на файл конфига

--- assets/snippets/tsvshop/addons/config/main.inc.php
+++ assets/snippets/tsvshop/addons/config/main.inc.php
@@ -6,16 +6,16 @@ $output.= ' <div class="tab-page" id="ShopConf">';
 $output.= '<h2 class="tab">' . $shop_lang['config_title'] . '</h2>';
 $output.= '<p><img src="'.TSVSHOP_SURL.'addons/config/img/configure.png" alt="'.$shop_lang['config_title'].'" class="icon" style="vertical-align: middle; text-align: left; " />'.$shop_lang['config_intro'];
 //Проверка файлов конфигурации на права для записи
-if (getfilechmod(TSVSHOP_PATH.'js/config.js') != "0666" || getfilechmod(TSVSHOP_PATH.'include/config.inc.php') != "0666") {
-    $output.= '<div class="notice">'.$shop_lang['config_chmod'].'</div><br />';
+if (getfilechmod(TSVSHOP_PATH.'js/config.js') < "0600" || getfilechmod(TSVSHOP_PATH.'include/config.inc.php') < "0600") {
+    $output.= '<div class="alert alert-warning">'.$shop_lang['config_chmod'].'</div><br />';
 }
 $output.= '
 <table cellpadding="0" cellspacing="0" class="actionButtons">
         <tr>
 <td>
 <ul class="actionButtons">
-                <li id="Button1"><a href="index.php?a=112&id=' . $moduleid . '"><img src="media/style'.$theme.'/images/icons/refresh.png"> '.$shop_lang['refresh'].'</a></li>
-                <li id="Button1"><a href="#" onclick="getform(\'/'.MGR_DIR.'/index.php\',document.getElementById(\'sconf\'),save_config_ok);"><img src="media/style'.$theme.'/images/icons/save.png"> '.$shop_lang['save'].'</a></li>
+                <li id="Button1"><a href="index.php?a=112&id=' . $moduleid . '"><i class="fa fa-refresh" aria-hidden="true"></i> '.$shop_lang['refresh'].'</a></li>
+                <li id="Button1"><a href="#" onclick="getform(\''.MODX_BASE_URL.MGR_DIR.'/index.php\',document.getElementById(\'sconf\'),save_config_ok);"><i class="fa fa-floppy-o" aria-hidden="true"></i> '.$shop_lang['save'].'</a></li>
             </ul>
 </td>
             <td id="report"></td>
@@ -23,7 +23,7 @@ $output.= '
     </table>';
 $output.= '
 <div id="sconf">
-<form action="javascript:getform(\'/'.MGR_DIR.'/index.php\',document.getElementById(\'sconf\'),save_config_ok);" name="sconf">
+<form action="javascript:getform(\''.MODX_BASE_URL.MGR_DIR.'/index.php\',document.getElementById(\'sconf\'),save_config_ok);" name="sconf">
 <input type="hidden" id="a" name="a" value="' . $modulea . '" />
 <input type="hidden" id="id" name="id" value="' . $moduleid . '" />
 <input type="hidden" id="act" name="act" value="saveconfig" />';
