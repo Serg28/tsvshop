f67fc6a7025e33c7b53d836e0d6b4de3b265ac31
Sergey <privat_tel@mail.ru>
2018-12-13 11:52:10 +0200

    v5.4.5 Правка путей к css и js в модуле

--- assets/snippets/tsvshop/admin/template/header.inc.php
+++ assets/snippets/tsvshop/admin/template/header.inc.php
@@ -8,31 +8,31 @@ $output .= '
         <link rel="stylesheet" type="text/css" href="media/style' . $theme . '/style.css" />
 		
         <!-- jeasyui -->
-        <link rel="stylesheet" type="text/css" href="/assets/js/easy-ui/themes/modxevo/easyui.css">
-	    <link rel="stylesheet" type="text/css" href="/assets/js/easy-ui/themes/icon.css">     
-	    <script type="text/javascript" src="/assets/js/jquery.min.js" language="javascript" ></script> 	
-	    <script type="text/javascript" src="/assets/js/easy-ui/locale/easyui-lang-ru.js"></script>
-	    <script type="text/javascript" src="/assets/js/easy-ui/jquery.easyui.min.js"></script>
-	    <script type="text/javascript" src="/assets/js/easy-ui/plugins/jquery.edatagrid.js"></script>
+        <link rel="stylesheet" type="text/css" href="'.MODX_BASE_URL.'assets/js/easy-ui/themes/modxevo/easyui.css">
+	    <link rel="stylesheet" type="text/css" href="'.MODX_BASE_URL.'assets/js/easy-ui/themes/icon.css">     
+	    <script type="text/javascript" src="'.MODX_BASE_URL.'assets/js/jquery.min.js" language="javascript" ></script> 	
+	    <script type="text/javascript" src="'.MODX_BASE_URL.'assets/js/easy-ui/locale/easyui-lang-ru.js"></script>
+	    <script type="text/javascript" src="'.MODX_BASE_URL.'assets/js/easy-ui/jquery.easyui.min.js"></script>
+	    <script type="text/javascript" src="'.MODX_BASE_URL.'assets/js/easy-ui/plugins/jquery.edatagrid.js"></script>
 
 
         <script type="text/javascript" src="media/script/tabpane.js"></script>
-		<link rel="stylesheet" type="text/css" href="../assets/snippets/tsvshop/admin/libs/datetimepicker/css/datepicker.css"/>
+		<link rel="stylesheet" type="text/css" href="'.MODX_BASE_URL.'assets/snippets/tsvshop/admin/libs/datetimepicker/css/datepicker.css"/>
 
         <script type="text/javascript" src="media/script/mootools/mootools.js"></script>
 
         <script type="text/javascript" src="media/script/mootools/moodx.js"></script>
         <script type="text/javascript" src="media/calendar/datepicker.js"></script>
-        <script src="../assets/snippets/tsvshop/admin/libs/datagrid/tablefilter_all_min.js" language="javascript" type="text/javascript"></script>
+        <script src="'.MODX_BASE_URL.'assets/snippets/tsvshop/admin/libs/datagrid/tablefilter_all_min.js" language="javascript" type="text/javascript"></script>
         <!-- Additional imported module needed for this demo  -->
-        <script src="../assets/snippets/tsvshop/admin/libs/datagrid/sortabletable.js" language="javascript" type="text/javascript"></script>
-        <script src="../assets/snippets/tsvshop/admin/libs/datagrid/tfAdapter.sortabletable.js" language="javascript" type="text/javascript"></script>
-        <script src="../assets/snippets/tsvshop/admin/libs/datagrid/TF_Modules/tf_paging.js" language="javascript" type="text/javascript"></script>
-        <link rel="stylesheet" type="text/css" href="../assets/snippets/tsvshop/admin/libs/datagrid/filtergrid.css">
-        <link rel="stylesheet" type="text/css" href="../assets/snippets/tsvshop/admin/template/themes/'.$moduletheme.'.css" />
+        <script src="'.MODX_BASE_URL.'assets/snippets/tsvshop/admin/libs/datagrid/sortabletable.js" language="javascript" type="text/javascript"></script>
+        <script src="'.MODX_BASE_URL.'assets/snippets/tsvshop/admin/libs/datagrid/tfAdapter.sortabletable.js" language="javascript" type="text/javascript"></script>
+        <script src="'.MODX_BASE_URL.'assets/snippets/tsvshop/admin/libs/datagrid/TF_Modules/tf_paging.js" language="javascript" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="'.MODX_BASE_URL.'assets/snippets/tsvshop/admin/libs/datagrid/filtergrid.css">
+        <link rel="stylesheet" type="text/css" href="'.MODX_BASE_URL.'assets/snippets/tsvshop/admin/template/themes/'.$moduletheme.'.css" />
 
         <!-- /grid -->
-		<script type="text/javascript" src="../assets/snippets/tsvshop/js/core.js"></script>
+		<script type="text/javascript" src="'.MODX_BASE_URL.'assets/snippets/tsvshop/js/core.js"></script>
 
 		</head>
         <body>
