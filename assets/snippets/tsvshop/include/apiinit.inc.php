56394643038383c1680e71d53bf4ee0afd1c3d54
serg <tsv.art.com@gmail.com>
2020-04-13 19:22:12 +0300

    v5.4.5 инициализация аякса с помощью index.php

--- assets/snippets/tsvshop/include/apiinit.inc.php
+++ assets/snippets/tsvshop/include/apiinit.inc.php
@@ -1,52 +1,47 @@
 <?php
-
 define('IN_TSVSHOP_MODE', true);
-//if (!$_api_path = $_SERVER["DOCUMENT_ROOT"]) {
-    if (!defined('DIRECTORY_SEPARATOR')) {
+define('MODX_API_MODE', true);
+//define('IN_MANAGER_MODE', true);
+
+if (!defined('DIRECTORY_SEPARATOR')) {
         define('DIRECTORY_SEPARATOR', "/");
     }
     $array     = explode(DIRECTORY_SEPARATOR, dirname(__FILE__));
     $size      = sizeof($array);
     $p1        = array_slice($array, 0, ($size - 4));
-    $_api_path = implode(DIRECTORY_SEPARATOR, $p1);
-//}
-$_api_path = $_api_path . "/";
+    $_api_path = implode(DIRECTORY_SEPARATOR, $p1).DIRECTORY_SEPARATOR;
 
-if (file_exists($_api_path . "assets/cache/siteManager.php")) {
-    include_once($_api_path . "assets/cache/siteManager.php");
-} else {
-    define('MGR_DIR', 'manager');
-}
-
-global $modx;
-require_once($_api_path . MGR_DIR . '/includes/protect.inc.php');
-include_once($_api_path . MGR_DIR . '/includes/config.inc.php');
-include_once($_api_path . MGR_DIR . '/includes/document.parser.class.inc.php');
-
-function executeDocument($docid = 0) {
-    global $modx;
-    ob_start();
-    $tmp            = $_REQUEST['id']; // save old id
-    $_REQUEST['id'] = $docid;
-    $modx->executeParser();
-    $html           = ob_get_contents();
-    ob_end_clean();
-    $_REQUEST['id'] = $tmp; // restore old id
-    return $html;
-}
-session_write_close();
 session_set_cookie_params("", '/');
-session_name($site_sessionname);
-session_start();
-//$modx = new MODxAPI();
-$modx                  = new DocumentParser;
+include_once($_api_path . "/index.php");
+
 $modx->db->connect();
-$modx->getSettings();
-$modx->minParserPasses = 2;
+if (empty ($modx->config)) {
+    $modx->getSettings();
+}
+
+
 $modx->invokeEvent("OnWebPageInit");
+
+if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || (strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')) {
+    $modx->sendRedirect($modx->config['site_url']);
+}
+////// для режима менеджера
+/*if (IN_MANAGER_MODE != "true" || empty($modx) || !($modx instanceof DocumentParser)) {
+    die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODX Content Manager instead of accessing this file directly.");
+}
+if (!$modx->hasPermission('exec_module')) {
+    header("location: " . $modx->getManagerPath() . "?a=106");
+}*/
+if (!is_array($modx->event->params)) {
+    $modx->event->params = array();
+}
+/*
+if (!isset($_SESSION['mgrValidated'])) {
+    die();
+}
+ */
 
 if (!$cache) {
     include_once MODX_BASE_PATH . '/assets/snippets/tsvshop/include/cache.class.php';
     $cache = fileCache::GetInstance(3600, MODX_BASE_PATH . 'assets/cache/');
 }
-?>
